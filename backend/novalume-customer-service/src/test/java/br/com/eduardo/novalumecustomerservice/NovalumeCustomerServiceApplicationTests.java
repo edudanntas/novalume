db4e19bfb72277631bb0a167278d4d169acdcbd6
Eduardo Dantas <eduardodanttas@icloud.com>
2025-05-24 17:02:35 -0300

    feat: initialize Novalume Customer Service with configuration files and Docker setup

---
+++ backend/novalume-customer-service/src/test/java/br/com/eduardo/novalumecustomerservice/NovalumeCustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eduardo.novalumecustomerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovalumeCustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
